121b3a275680c842b75142ed8dd6b00227514288
Artem Rosnovsky <artem@rosnovsky.us>
2019-04-24 15:09:44 -0700

    basic notes list

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { API } from 'aws-amplify';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import './Home.css';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      notes: []
+    };
+  }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await this.notes();
+      this.setState({ notes });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  notes() {
+    return API.get('notes', '/notes');
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotesList(notes) {
+    return [{}].concat(notes).map((note, i) =>
+      i !== 0 ? (
+        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+          <ListGroupItem header={note.content.trim().split('\n')[0]}>
+            {'Created: ' + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        </LinkContainer>
+      ) : (
+        <LinkContainer key="new" to="/notes/new">
+          <ListGroupItem>
+            <h4>
+              <b>{'\uFF0B'}</b> Create a new note
+            </h4>
+          </ListGroupItem>
+        </LinkContainer>
+      )
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <h1>Your Notes</h1>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Noted</h1>
-          <p>A note taking app</p>
+        <div className="Home">
+          {this.props.isAuthenticated
+            ? this.renderNotes()
+            : this.renderLander()}
         </div>
       </div>
     );
